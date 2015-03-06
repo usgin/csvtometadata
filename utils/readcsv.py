cf1ec023f60f005ba8e5ea47ba1f303a9957b8c1
Christy Caudill Daugherty <christy.caudill@azgs.az.gov>
2015-03-06 15:04:12 -0700

    deleting old files

--- utils/readcsv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def csv_to_dict_reader(csv_file_path):
-    try:
-        return csv.DictReader(open(csv_file_path))
-    except IOError, ex:
-        raise ex
-    except Exception, ex:
-        raise ex
